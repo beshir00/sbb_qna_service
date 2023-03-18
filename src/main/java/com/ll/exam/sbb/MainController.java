f2d6677ca4fb8791afd103719fc78d0ad117d9d1
beshir00 <eik852@naver.com>
2023-03-18 23:15:29 +0900

    plus minus increase 테스트

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -1,6 +1,7 @@
 package com.ll.exam.sbb;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,5 +12,24 @@ public class MainController {
     @ResponseBody
     public String index(){
         return "안녕하세요.";
+    }
+
+    @GetMapping("/plus")
+    @ResponseBody
+    public int showPlus(int a, int b){
+        return a+b;
+    }
+
+    @GetMapping("/minus")
+    @ResponseBody
+    public int showMinus(int a, int b){
+        return a-b;
+    }
+    int increaseNo = -1;
+    @GetMapping("/increase")
+    @ResponseBody
+    public int showIncrease(){
+        increaseNo++;
+        return increaseNo;
     }
 }
