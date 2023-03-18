1cc9b5dfb740415e21db10efd30850fb6454374e
beshir00 <eik852@naver.com>
2023-03-18 10:10:33 +0900

    ResponseBody 테스트

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -2,12 +2,14 @@ package com.ll.exam.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
     @RequestMapping("/sbb")
-    public void index(){
-        System.out.println("index");
+    @ResponseBody
+    public String index(){
+        return "안녕하세요.";
     }
 }
