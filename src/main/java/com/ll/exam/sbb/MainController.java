e60cf05de04bcf92142d7235fa2025ed9ac65482
beshir00 <eik852@naver.com>
2023-03-18 09:54:13 +0900

    MainController 메소드 추가

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -1,7 +1,6 @@
 package com.ll.exam.sbb;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
