de6f081c577db1234c8c97f6c5c8f76a61fb70f2
beshir00 <eik852@naver.com>
2023-04-08 10:33:22 +0900

    question 상세페이지 내 답변 목록 구현

---
+++ src/main/java/com/ll/exam/sbb/answer/AnswerController.java
@@ -0,0 +1,31 @@
+package com.ll.exam.sbb.answer;
+
+import com.ll.exam.sbb.question.Question;
+import com.ll.exam.sbb.question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/answer")
+@Controller
+@RequiredArgsConstructor
+public class AnswerController {
+
+    private final QuestionService questionService;
+    private final  AnswerService answerService;
+
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Integer id, String content){
+        Question question = questionService.getQuestion(id);
+        answerService.create(question, content);
+
+
+        return "redirect:/question/detail/%d".formatted(id);
+
+    }
+
+
+}
